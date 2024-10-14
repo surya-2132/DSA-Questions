159b40d3773e8b1c83173b29c21679431472cf23
raj_surya.t <rajsurya2132@gmail.com>
2024-10-14 21:13:54 +0530

    Recursion FAQ's

---
+++ src/Recursion/SubSequencePattern/_1PowerSet.java
@@ -0,0 +1,62 @@
+package Recursion.SubSequencePattern;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class _1PowerSet {
+    public static void main(String[] args) {
+        int[] nums = new int[]{1,2,3};
+        System.out.println(powerSet(nums));
+    }
+
+    public static List<List<Integer>> powerSet(int[] nums) {
+        List<List<Integer>> ansList = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        recSet(0, list, ansList, nums, nums.length);
+        return ansList;
+    }
+
+    public static void recSet(int index, List<Integer> list, List<List<Integer>> ansList, int[] nums, int numsLength){
+
+        if(index == numsLength){
+            ansList.add(new ArrayList<>(list));
+            return;
+        }
+
+        recSet(index + 1, list, ansList, nums, numsLength);
+        list.add(nums[index]);
+
+        recSet(index + 1, list, ansList, nums, numsLength);
+        list.removeLast();
+
+    }
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        //your code goes here
+        List<List<Integer>> ansList = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        recCombination(0, list, ansList, target, candidates.length, candidates);
+        return ansList;
+    }
+
+    public void recCombination(int index, List<Integer> list,
+                               List<List<Integer>> ansList,
+                               int sum, int n, int[] nums){
+        if(sum == 0){
+            ansList.add(new ArrayList<>(list));
+            return;
+        }
+
+        if(sum < 0 || index == n){
+            return;
+        }
+
+
+        recCombination(index, list, ansList, sum - nums[index], n, nums);
+        list.add(nums[index]);
+
+        list.removeLast();
+        recCombination(index + 1, list, ansList, sum, n, nums);
+        return;
+    }
+}
