ca5d299bd4af69bda28bd67e353b680bf7ef2d03
raj_surya.t <rajsurya2132@gmail.com>
2025-12-08 14:53:13 +0530

    Adding product subarray sum problem

--- src/Arrays/Medium/MaximumProductSubarray.java
+++ src/Arrays/Medium/MaximumProductSubarray.java
@@ -1,74 +1,46 @@
 package Arrays.Medium;
 
-public class MaximumProductSubarray {
+public class MaximumProductSubArray {
     public static void main(String[] args) {
-        System.out.println(productSubArrBrute(new int[] {2,3,-2,4}));
-        System.out.println(productSubArrBetter(new int[] {2,3,-2,4}));
-        System.out.println(productSubArrOptimal(new int[] {2,3,-2,4}));
-
+        int[] nums = {2,0,-1,-4,2,0};
+        System.out.println("Brute: "+maximumProductBrute(nums));
+        System.out.println("Optimal: "+maximumProductOptimal(nums));
     }
 
-    public static int productSubArrBrute(int[] nums){
-        //2,3,-2,4
-        int max = Integer.MIN_VALUE;
-        for (int i = 0; i < nums.length; i++){
-            for(int j = i; j < nums.length; j++){
+
+    public static int maximumProductBrute(int[] nums){
+        // 2,0,-1,-4,2,0
+        int maxProd = Integer.MIN_VALUE;
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i+1; j < nums.length; j++){
                 int product = 1;
-                for(int k = i; k <= j; k++){
-                    product = product * nums[k];
+                for(int k = i; k <= j; k++) {
+                    product *= nums[k];
+                    maxProd = Math.max(maxProd, product);
                 }
-                max = Math.max(max, product);
             }
         }
-        return max;
+        return maxProd;
     }
 
-
-    public static int productSubArrBetter(int[] nums){
-        //2,3,-2,4
-        int max = Integer.MIN_VALUE;
-        for (int i = 0; i < nums.length; i++){
-            int product = 1;
-            for(int j = i; j < nums.length; j++){
-                product = product * nums[j];
-                max = Math.max(max, product);
-            }
-
-        }
-        return max;
-    }
-
-    public static int productSubArrOptimal(int[] nums){
-        int prefix = 1; //like left pointer
-        int suffix = 1; //like right pointer
-        int max = Integer.MIN_VALUE;    //track max between prefix and suffix
+    public static int maximumProductOptimal(int[] nums){
+        int prefixProd = 1;
+        int suffixProd = 1;
+        int maxProd = Integer.MIN_VALUE;
 
         for(int i = 0; i < nums.length; i++){
-
-            if(prefix == 0){  //while traversing any element is 0, reset prefix prod to 1
-                prefix = 1;
+            if(prefixProd == 0) {
+                prefixProd = 1;
             }
 
-            if(suffix == 0){  //while traversing any element is 0, reset suffix prod to 1
-                suffix = 1;
+            if(suffixProd == 0){
+                suffixProd = 1;
             }
 
-            //multiplying the current ith value with prev calculated prefix/suffix
-            prefix = prefix * nums[i];
-            suffix = suffix * nums[nums.length-1-i];
-
-            max = Math.max(max, Math.max(prefix, suffix));
+            prefixProd = prefixProd * nums[i];
+            suffixProd = suffixProd * nums[nums.length -i-1];
+            maxProd = Math.max(maxProd, Math.max(prefixProd, suffixProd));
         }
-
-        //did this same calculation in one go (suffix line, n-1-p)
-        // for(int s = nums.length-1; s >= 0; s--){
-        //     if(nums[s] == 0){
-        //         suffix = 1;
-        //     }
-        //     suffix = suffix* nums[s];
-        //     max = Math.max(max, suffix);
-        // }
-
-        return max;
+        return maxProd;
     }
 }
